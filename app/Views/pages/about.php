ee2f818ae546a412097aa153b9b96a3005ff599f
andrekristiantoro <kristiantoro90@gmail.com>
2020-10-28 16:23:43 +0700

    "rekweb2020_183040032_ci4app"

---
+++ app/Views/pages/about.php
@@ -0,0 +1,12 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis praesentium deserunt iusto reprehenderit suscipit natus, voluptas sequi tenetur nulla sunt veniam dolore est recusandae facilis quos assumenda reiciendis temporibus autem.</p>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
